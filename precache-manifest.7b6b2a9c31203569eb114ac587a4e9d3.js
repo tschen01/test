9a7aa249b8391ccbffd39731e1a04e09f5f56ed0
root <root@DESKTOP-ALP2BMH.localdomain>
2020-03-25 20:47:34 -0600

    Updates

---
+++ precache-manifest.7b6b2a9c31203569eb114ac587a4e9d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a609e182c1696d3b06a70e2b5d1f5a",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a35f316bd9ba2446920a",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ade278507ccc286d5c6f",
+    "url": "/test/static/js/2.5eb979f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.5eb979f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35f316bd9ba2446920a",
+    "url": "/test/static/js/main.4fd84e9d.chunk.js"
+  },
+  {
+    "revision": "a2a118daa8ca02cff825",
+    "url": "/test/static/js/runtime-main.5ebfacb6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
