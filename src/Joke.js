60b21f7f3291a7f5fab247e8cd429c145978c8f5
root <root@DESKTOP-ALP2BMH.localdomain>
2020-03-25 21:22:52 -0600

    final

--- src/Joke.js
+++ src/Joke.js
@@ -47,9 +47,7 @@ export default class Joke extends Component {
       <div className="Joke">
         <div className="Joke-buttons">
           <i className="fas fa-arrow-up" onClick={this.props.upvote}></i>
-          <span className="Joke-votes" style={{ borderColor: this.getColor() }}>
-            {this.props.votes}
-          </span>
+          <div>😂</div>
           <i className="fas fa-arrow-down" onClick={this.props.downvote}></i>
         </div>
         <div className="Joke-text">{this.props.text}</div>
